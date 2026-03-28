cf2d5e6c1e7a2f88e75f2307266914f66923a827
kemmis <elvin11_99@hotmail.com>
2026-03-28 15:15:33 -0500

    add connection to notion

---
+++ api/notion.js
@@ -0,0 +1,19 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Authorization,Content-Type,Notion-Version');
+  if (req.method === 'OPTIONS') return res.status(200).end();
+
+  const path = req.url.replace('/api/notion', '');
+  const notionRes = await fetch('https://api.notion.com/v1' + path, {
+    method: req.method,
+    headers: {
+      'Authorization': req.headers['authorization'],
+      'Notion-Version': '2022-06-28',
+      'Content-Type': 'application/json'
+    },
+    body: req.method !== 'GET' ? JSON.stringify(req.body) : undefined
+  });
+  const data = await notionRes.json();
+  res.status(notionRes.status).json(data);
+}
